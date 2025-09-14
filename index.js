a416e4b15cabb4432ba58eb8ada9897811244fa3
Connor Raetz <raetz2008@web.de>
2025-09-14 20:26:05 +0200

    Alle Coins funktionieren + besser

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 
+const allCoins = document.querySelectorAll('.coin-button');
 const fiveCent = document.getElementById('five-cent');
 const tenCent = document.getElementById('ten-cent');
 
 let timeValue = 0;
 
-fiveCent.onclick = function () {
-    console.log(timeValue);
-    timeValue = timeValue + 5;
-    console.log(timeValue);
+allCoins.forEach(coin => {
+    coin.addEventListener('click', (event) => {
+        const clickedId = event.target.id;
 
-}
-tenCent.onclick = function () {
-    console.log(timeValue);
-    timeValue = timeValue + 10;
-    console.log(timeValue);
-}
+        switch (clickedId) {
+            case 'five-cent':
+                timeValue += 5;
+                break;
+            case 'ten-cent':
+                timeValue += 10;
+                break;
+            case 'fifty-cent':
+                timeValue += 50;
+                break;
+            case 'one-euro':
+                timeValue += 100;
+                break;
+            case 'two-euro':
+                timeValue += 200;
+                break;
+        }
+        console.log(timeValue);
+    })
+})
