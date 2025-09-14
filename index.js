c15e16ef7794f1c822459509869d721ab5b5481c
Connor Raetz <raetz2008@web.de>
2025-09-14 20:17:24 +0200

    10 Cent funktioniert

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 
 const fiveCent = document.getElementById('five-cent');
+const tenCent = document.getElementById('ten-cent');
 
 let timeValue = 0;
 
 fiveCent.onclick = function () {
     console.log(timeValue);
     timeValue = timeValue + 5;
+    console.log(timeValue);
+
+}
+tenCent.onclick = function () {
+    console.log(timeValue);
+    timeValue = timeValue + 10;
     console.log(timeValue);
 }
